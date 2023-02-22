8bf2efc6753f7ad4affd458acfaddeb64b8f5888
MariaGF <miriangimenez@gmail.com>
2023-02-22 20:47:23 +0100

    changes Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Navbar from './components/partials/Navbar/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Navbar />
+      <Navbar />
+        <Routes>          
             <Route path='/' element={<Home></Home>} />
             <Route path='/about' element={<About></About>} />
-            <Route path='/contact' element={<Contact></Contact>} />
-          <Footer />
+            <Route path='/contact' element={<Contact></Contact>} />      
         </Routes>
+        <Footer />
       </Router>  
     </>
   )
