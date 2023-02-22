37769eafe3195658fc9a694901797399f5d64fd9
mintybubblegum <anamaria.tz@hotmail.com>
2023-02-22 19:01:39 +0100

    merged guillemDev to noaDev

--- backend/database/factories/ServiceFactory.php
+++ backend/database/factories/ServiceFactory.php
@@ -14,7 +14,7 @@ class ServiceFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition(): array 
     {
         return [
           'name' => fake()->name(),
