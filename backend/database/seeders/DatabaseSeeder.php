c424ab204c5ee51383e4ca71f9cad03efe053c32
Guillem <guillemmj33@gmail.com>
2023-02-22 19:00:31 +0100

    service data user seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,15 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+      $this->call([
+        ServiceSeeder::class
+      ]);
 
-        // // \App\Models\User::factory()->create([
-        // //     'name' => 'Test User',
-        // //     'email' => 'test@example.com',
-        // // ]);
-
-        $this->call([
-          ServiceSeeder::class
+      $this->call([
+        UserSeeder::class
       ]);
     }
 }
