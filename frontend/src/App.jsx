7b00bfa184eb511759954df5d5d8c4ba1bd6b0af
jaume vidal <jaumevibu@gmail.com>
2023-02-22 20:51:00 +0100

    before testing tests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Navbar from "./components/partials/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Navbar />
           <Route path="/" element={<Home></Home>} />
           <Route path="/about" element={<About></About>} />
           <Route path="/contact" element={<Contact></Contact>} />
-          <Footer />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
