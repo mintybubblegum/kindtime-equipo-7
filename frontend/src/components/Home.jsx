2fc2009eeb83cdb6233a9b80682bdfccd7da929e
jaume vidal <jaumevibu@gmail.com>
2023-02-23 11:00:55 +0100

    added tests for Seachbar and Home Components

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,10 @@ export default function Home() {
   return (
     <div className="px-6 mt-6 mb-6 flex flex-col justify-center gap-6 md:px-12 lg:px-40">
       <center>
-        <h1 className="text-2xl font-light italic text-dark-blue">
+        <h1
+          className="text-2xl font-light italic text-dark-blue"
+          data-testid="h1Slogan"
+        >
           We share <span className="font-bold">life</span> with your local
           elders. Join the{" "}
           <span className="underline decoration-yellow decoration-2 underline-offset-8">
