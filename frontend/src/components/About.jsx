d7ff440dc4be99395a4578efa4a43d739d4f2185
ChaanChanChan <alexandertakepics@gmail.com>
2023-02-23 14:18:53 +0100

    first commit

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import AboutCard from './partials/AboutCard'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <AboutCard />
+    </>
   )
 }
---
+++ frontend/src/components/partials/AboutCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AboutImg from '../../assets/about.jpg'
+
+export default function AboutCard() {
+    return (
+        <div className="flex flex-col md:flex-row my-5 m-auto">
+            <img className="" src={AboutImg} alt="img" />
+            <div className="p-5 bg-blue w-auto">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About Us</h5>
+                <p className="mb-3 font-normal text-white dark:text-gray-500">We are the first community dedicated 100% to the care of our seniors in a free way. We share your life and help you to make your day-to-day life easier.</p>
+
+                <p className="mb-3 font-normal text-white dark:text-gray-500">Our values are based on face-to-face or digital support, depending on the need they want to cover. Through love and help, we add value. We do it locally, from your neighbourhood, from any location, so that no one is left without what they need.</p>
+
+                <p className="mb-3 font-normal text-white dark:text-gray-500">Join our community and bring what you do best to the person who needs it.</p>
+            </div>
+        </div>
+    )
+}
