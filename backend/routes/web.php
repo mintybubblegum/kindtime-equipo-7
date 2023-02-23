042a3169aa92c9958abe15dbfdd2b63fa525a2f2
Uxoa <palomita@gmail.com>
2023-02-23 11:51:55 +0100

    postman training Api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,12 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
 
 
-// Route::get('/{path}', function () {
-//     return view('welcome');
-// })->where('path' , '.*');
+
 
 Route::get('/services', 'Api\ServiceController@index');
 Route::post('/store', 'Api\ServiceController@store');
-Route::delete('/delete/{id}', 'Api\ServiceController@destroy');
+Route::delete('/services/{id}', 'Api\ServiceController@destroy');
 Route::get('edit/{id}', 'Api\ServiceController@edit');
 Route::put('update/{id}', 'Api\ServiceController@update');
