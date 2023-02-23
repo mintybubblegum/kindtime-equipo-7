8f7b1a7687d44981626f63c89cfeabd058113f2f
Uxoa <palomita@gmail.com>
2023-02-23 13:50:30 +0100

    a little break while merging

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
 
+Route::get('/', [App\Http\Controllers\Api\ServiceController::class, 'index'])->name('servicesApi');
+
+
 Route::apiResource('services', ServiceController::class );
