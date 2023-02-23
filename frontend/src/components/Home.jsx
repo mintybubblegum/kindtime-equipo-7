b3af88397568f1c12efa069d6232c884eb5a8d0b
Guillem <guillemmj33@gmail.com>
2023-02-23 14:57:59 +0100

    view map ok 2

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect }  from 'react'
-import Searchbar from './partials/Searchbar'
-import Card from '../components/partials/Card'
 import axios from 'axios'
 
 export default function Home() {
@@ -53,26 +51,26 @@ export default function Home() {
         .slice(0,6)
         .map((service) => {
           return(
-              <div key={service.id} className="max-w-sm sm:max-w-xs bg-dark-blue border border-gray rounded-lg shadow dark:bg-blue dark:border-gray-700">
-                <a href="#">
-                    <img className="rounded-t-lg" src={service.serviceImg} alt="img" />
-                </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{service.title}</h5>
+            <div key={service.id} className="max-w-sm sm:max-w-xs bg-dark-blue border border-gray rounded-lg shadow dark:bg-blue dark:border-gray-700">
+              <a href="#">
+                  <img className="rounded-t-lg" src={service.serviceImg} alt="img" />
+              </a>
+              <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{service.title}</h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{service.descriptionSm}</p>
+                  <div className="flex justify-between">
+                    <a href="/service" className="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-dark-blue rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        See more
+                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{service.descriptionSm}</p>
-                    <div className="flex justify-between">
-                      <a href="/service" className="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-dark-blue rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                          See more
-                          <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                      </a>
-                      <a href="/service" className="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-dark-blue bg-yellow rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                          Contact
-                      </a>
-                    </div>
-                </div>
+                    <a href="/service" className="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-dark-blue bg-yellow rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Contact
+                    </a>
+                  </div>
               </div>
+            </div>
           )
         })}
       </div>
