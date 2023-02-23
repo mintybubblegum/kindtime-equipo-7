f6af1cc6fd4dc7b5581adca89e0b759df2d1060a
Guillem <guillemmj33@gmail.com>
2023-02-23 13:14:13 +0100

    navbar collapse ok

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServiceController;
 
 
-Route::get('/', [App\Http\Controllers\Api\ServiceController::class, 'index'])->name('servicesApi');
-
-
-Route::get('/services', 'Api\ServiceController@index');
-Route::post('/store', 'Api\ServiceController@store');
-Route::delete('/delete/{id}', 'Api\ServiceController@destroy');
-Route::get('edit/{id}', 'Api\ServiceController@edit');
-Route::put('update/{id}', 'Api\ServiceController@update');
+Route::apiResource('services', ServiceController::class);
