fec06cb1426fd5c4494e3273290e77b58e823214
Uxoa <palomita@gmail.com>
2023-02-23 11:58:29 +0100

    fixing the right way with TDD Api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [App\Http\Controllers\Api\ServiceController::class, 'index'])->name('servicesApi');
+
+
+Route::get('/services', 'Api\ServiceController@index');
+Route::post('/store', 'Api\ServiceController@store');
+Route::delete('/delete/{id}', 'Api\ServiceController@destroy');
+Route::get('edit/{id}', 'Api\ServiceController@edit');
+Route::put('update/{id}', 'Api\ServiceController@update');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,9 +5,3 @@ use App\Http\Controllers\Api\ServiceController;
 
 
 
-
-Route::get('/services', 'Api\ServiceController@index');
-Route::post('/store', 'Api\ServiceController@store');
-Route::delete('/services/{id}', 'Api\ServiceController@destroy');
-Route::get('edit/{id}', 'Api\ServiceController@edit');
-Route::put('update/{id}', 'Api\ServiceController@update');
