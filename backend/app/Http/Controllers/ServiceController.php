5e9c35e439cbb502afd5b899bd3d09cd88a27071
mintybubblegum <anamaria.tz@hotmail.com>
2023-02-24 01:30:56 +0100

    merged main to develop

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Service;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller; 
 
 /**
  * Class ServiceController
